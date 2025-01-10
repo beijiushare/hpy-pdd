1e5ad3366609c84d6fdf012d165faafedd09f613
beijiushare <1292396120@qq.com>
2025-01-10 17:13:03 +0800

    Update big_red_envelope.vue

--- src/components/big_red_envelope.vue
+++ src/components/big_red_envelope.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="big_red_envelope">
     <p1>北酒的红包</p1>
-    <p2>新年快乐&nbsp;&nbsp;金蛇贺岁</p2>
-    <img src="../img/gold_snake.webp" />
+    <p2 v-if="showmessage">新年快乐&nbsp;&nbsp;金蛇贺岁</p2>
+    <img src="../img/gold_snake.webp" v-if="showmessage" />
     <div class="wrapper" id="wrap">
       <div class="arc-continer" id="continer"></div>
     </div>
-    <div class="circle">
+    <div class="circle" @click="Receive($event)" v-if="showmessage">
       <h1>開</h1>
     </div>
+    <p3 v-if="showmoney">恭喜你抢到{{ money }}元红包</p3>
   </div>
 </template>
 
 <script>
-export default { name: "big_red_envelope" };
+export default {
+  name: "big_red_envelope",
+  data() {
+    return {
+      showmessage: true,
+      showmoney: false,
+      money: 100000000,
+    };
+  },
+  methods: {
+    Receive(event) {
+      event.currentTarget.classList.toggle("rotate"); // 切换.rotate类
+
+      setTimeout(() => {
+        this.showmessage = false; // 隐藏红包
+        this.showmoney = true; // 重置红包金额
+      }, 1500);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -51,7 +71,7 @@ p2 {
 img {
   height: 38%;
   width: auto;
-  margin-top: 2%;
+  margin-top: 3%;
 }
 
 .wrapper {
@@ -80,7 +100,7 @@ img {
   background-color: rgba(255, 255, 255, 0);
   position: absolute;
   /* 绘制为椭圆 */
-  border: #ffffff 2px solid;
+  border: #ffffff 1px solid;
   border-radius: 100% /100%;
   /* top 为  椭圆高度 - arc-continer高度*/
   top: 180px;
@@ -99,10 +119,34 @@ img {
   bottom: 7%;
 }
 
+.rotate {
+  animation: rotate linear 0.3s infinite;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotateY(180deg);
+  }
+  to {
+    transform: rotateY(360deg);
+  }
+}
+
 h1 {
   font-size: 35px;
   text-align: center; /* 水平居中 */
   margin: 0; /* 去除默认的外边距 */
   color: black;
+}
+
+p3 {
+  position: absolute;
+  font-size: 25px;
+  text-align: center; /* 水平居中 */
+  margin: 0; /* 去除默认的外边距 */
+  color: rgb(255, 255, 255);
+  margin-bottom: 30%;
+  margin-left: 5%;
+  margin-right: 5%;
 }
 </style>
